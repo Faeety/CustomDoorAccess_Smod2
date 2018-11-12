a16fcce01ad5b9530f188405d8b20708ea0d3d85
Faeety <42270395+Faeety@users.noreply.github.com>
2018-11-13 00:25:58 +0100

    new repo

---
+++ CustomDoorAccess/CustomDoorAccess.cs
@@ -0,0 +1,40 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Events;
+using System;
+using System.Collections.Generic;
+
+namespace CustomDoorAccess
+{
+    [PluginDetails(
+    author = "Faety",
+    description = "Custom your door access, doorid:card",
+    id = "faety.door.access",
+    name = "Custom Door Access",
+    SmodMajor = 3,
+    SmodMinor = 0,
+    SmodRevision = 0,
+    version = "1.0"
+    )]
+
+    public class CustomDoorAccess : Plugin
+    {
+        public override void Register()
+        {
+            this.AddEventHandlers(new EventHandler(this), Priority.High);
+            AddConfig(new Smod2.Config.ConfigSetting("cda_access_set", new Dictionary<string, string>(), true, Smod2.Config.SettingType.DICTIONARY, true, "Set access."));
+            AddConfig(new Smod2.Config.ConfigSetting("cda_revoke_all", false, true, Smod2.Config.SettingType.BOOL, true, "Revoke the access for all the other cards ?"));
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("Plugin enabled.");
+        }
+
+        public override void OnDisable()
+        {
+            this.Info("Plugin disabled.");
+        }
+
+    }
+}
---
+++ CustomDoorAccess/EventHandlers.cs
@@ -0,0 +1,49 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoorAccess
+{
+    class EventHandler : IEventHandlerDoorAccess
+    {
+        Plugin plugin;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        Dictionary<string, string> access = ConfigManager.Manager.Config.GetDictValue("cda_access_set");
+        bool revokeAll = ConfigManager.Manager.Config.GetBoolValue("cda_revoke_all", false);
+
+        public void OnDoorAccess(PlayerDoorAccessEvent ev)
+        {
+            foreach(KeyValuePair<string, string> x in access)
+            {
+                if(ev.Door.Name == x.Key)
+                {
+                    int itemID;
+                    if (Int32.TryParse(x.Value, out itemID))
+                    {
+                        if (ev.Player.GetCurrentItem().ItemType.Equals((ItemType)itemID))
+                        {
+                            ev.Allow = true;
+                        }
+                        else if (revokeAll)
+                        {
+                            ev.Allow = false;
+                        }
+                    }
+                    else
+                    {
+                        plugin.Info("There was a problem with the ItemID");
+                    }
+                }
+            }
+        }
+    }
+}
